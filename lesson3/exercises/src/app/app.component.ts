2c9d62c87f31340f7f299939e86f304edadf4632
mpbelz <mpbelz@gmail.com>
2021-12-16 13:22:30 -0600

    Angular Exercises Lesson3

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -12,4 +12,61 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+  takeOffEnabled = true;
+
+  moveRocket(rocketImage, direction){
+    if (direction === 'right') {
+      let movement = parseInt(rocketImage.style.left) + 10 + 'px';
+      rocketImage.style.left = movement;
+      this.width = this.width + 10000;
+    }
+    if (direction === 'left') {
+      let movement = parseInt(rocketImage.style.left) - 10 + 'px';
+      rocketImage.style.left = movement;
+    }
+    if (direction === 'down') {
+      let movement = parseInt(rocketImage.style.bottom) - 10 + 'px';
+      rocketImage.style.bottom = movement;
+      this.height -= 10000;
+    } 
+    if (direction === 'up') {
+      let movement = parseInt(rocketImage.style.bottom) + 10 + 'px';
+      rocketImage.style.bottom = movement;
+      this.height += 10000; 
+    }
+  }
+
+  handleTakeOff() {
+    let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
+    if (result) {
+       this.color = 'blue';
+       this.height = 10000;
+       this.width = 0;
+       this.message = 'Shuttle in flight.';
+    }
+    this.takeOffEnabled = false;
+ }
+  handleLand(rocketImage) {
+    window.alert("The shuttle is landing. Landing gear engaged");
+    this.color = 'green';
+    this.height = 0;
+    this.width = 0;
+    this.message = 'This shuttle has landed.';
+    rocketImage.style.bottom = '0px';
+    rocketImage.style.left = '0px';
+    this.takeOffEnabled = true;
+  }
+
+  handleMissionAbort(rocketImage) {
+    let result = window.confirm("Are you sure you want to end the mission?");
+    if (result) {
+      this.color = "red";
+      this.height = 0;
+      this.width = 0;
+      this.message = "Mission aborted.";
+      rocketImage.style.bottom = '0px';
+      rocketImage.style.left = '0px';
+      this.takeOffEnabled = true;
+    }
+  }
 }
